eeb0437b1949ed5edbe3544919478d1b9d9122fe
Sujit XPS Ubuntu <contact@sujitsagar.com>
2015-06-23 19:48:57 +0530

    added 24 cols

--- package.js
+++ package.js
@@ -6,7 +6,7 @@
 Package.describe({
   name: 'sujitsagar:bootstrap',  // http://atmospherejs.com/twbs/bootstrap
   summary: 'The most popular front-end framework for developing responsive, mobile first projects on the web.',
-  version: '3.3.5',
+  version: '1.1.1',
   git: 'https://github.com/sujitsagar/bootstrap.git'
 });
 
@@ -14,11 +14,11 @@ Package.onUse(function (api) {
   api.versionsFrom('METEOR@1.0');
   api.use('jquery', 'client');
   api.addFiles([
-    'dista/fonts/glyphicons-halflings-regular.eot',
+    /*'dista/fonts/glyphicons-halflings-regular.eot',
     'dista/fonts/glyphicons-halflings-regular.svg',
     'dista/fonts/glyphicons-halflings-regular.ttf',
     'dista/fonts/glyphicons-halflings-regular.woff',
-    'dista/fonts/glyphicons-halflings-regular.woff2',
+    'dista/fonts/glyphicons-halflings-regular.woff2',*/
     'dista/css/bootstrap.css',
     'dista/js/bootstrap.js'
   ], 'client');
